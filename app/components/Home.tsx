b6760d00499970cdc71bfec09f685133833cfc9b
pansongkui001 <pansongkui001@ke.com>
2020-10-25 21:19:11 +0800

    feat: update

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
+import React, { useState } from 'react';
+import Posion from '../config/posion.json';
 import styles from './Home.css';
 
+const getRandom = () => {
+  return Math.floor(Math.random() * Posion.length);
+};
+
 export default function Home(): JSX.Element {
+  const [posion, setPosion] = useState(Posion[getRandom()]);
+  const onChange = () => {
+    setPosion(Posion[getRandom()]);
+  };
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+      <div className={styles.content} onClick={onChange}>
+        <p className={styles.title}>今日份毒鸡汤：</p>
+        <p className={styles.posion}>{posion}</p>
+      </div>
+      <h4 className={styles.footer}>©2020 Powered by Bill</h4>
     </div>
   );
 }
